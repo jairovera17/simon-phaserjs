cebcfaaee50c026309d23798944467276928b3ea
jairovera17 <jairovera17@gmail.com>
2020-06-11 21:10:43 -0500

    Updates

---
+++ precache-manifest.34af4d3c086f81108c8112d4f88bfdaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5e01b131ad00e0d9a79bcfcbaf50e8",
+    "url": "/simon-phaser/index.html"
+  },
+  {
+    "revision": "84400a4d26674acb34b6",
+    "url": "/simon-phaser/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93240f4362a9038f1ea5",
+    "url": "/simon-phaser/static/js/2.5caca5bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simon-phaser/static/js/2.5caca5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84400a4d26674acb34b6",
+    "url": "/simon-phaser/static/js/main.20adfc57.chunk.js"
+  },
+  {
+    "revision": "43ca6443fbecfb2c7191",
+    "url": "/simon-phaser/static/js/runtime-main.bf555e26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simon-phaser/static/media/logo.5d5d9eef.svg"
+  }
+]);
